ab909005a347c02c29cd5c9d35f907a4a1790519
Ritik <ritik78699@gmail.com>
2024-09-11 19:16:23 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const[color1,setColor1]=useState(0);
+  const[color2,setColor2]=useState(0);
+  const[color3,setColor3]=useState(0);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<>
+    <div class="container">
+      <h1>Color Picker Using RGB </h1>
+      <input type='range' min="0" max="255" onChange={e=>
+      setColor1(e.target.value)
+  } value={color1} /> 
+     <p>value of R {color1}</p>
+      <input type='range' min="0" max="255" onChange={e=>
+      setColor2(e.target.value)} value={color2} />
+      <p>value of G {color2}</p>
+      <input type='range' min="0" max="255" onChange={e=>
+      setColor3(e.target.value)} value={color3} /> 
+      <p>value of B {color3}</p>
+      <h4> value of RGB {`(${color1},${color2},${color3})`}</h4>
+      <div 
+      className="box" 
+      style={{ backgroundColor: `rgb(${color1},${color2},${color3})`,
+       border: "4px solid black", 
+       width: "200px", 
+       height: "100px" }}>
+      </div>
+      
     </div>
-  );
+    <footer> Made By Ritik Trivedi</footer>
+</>  );
 }
 
 export default App;
